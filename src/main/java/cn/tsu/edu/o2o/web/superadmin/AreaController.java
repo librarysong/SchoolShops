72de466bbda3eee9633051332ca8bd18013e4e48
swf <1101699259@qq.com>
2018-03-24 09:36:40 +0800

    添加日志支持

--- src/main/java/cn/tsu/edu/o2o/web/superadmin/AreaController.java
+++ src/main/java/cn/tsu/edu/o2o/web/superadmin/AreaController.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,12 +24,17 @@ import cn.tsu.edu.o2o.service.AreaService;
 @Controller
 @RequestMapping("/superadmin")
 public class AreaController {
+	
+	Logger logger=LoggerFactory.getLogger(AreaController.class);
 	@Autowired
 	private AreaService areaService;
 
 	@RequestMapping(value="/listarea",method=RequestMethod.GET)
 	@ResponseBody
 	private Map<String, Object> listArea() {
+		
+		logger.info("===========start===========");
+		long startTime=System.currentTimeMillis();
 	    Map<String, Object> modelMap = new HashMap<String, Object>();
         List<Area> list=new ArrayList<Area>();
         try {
@@ -40,6 +47,10 @@ public class AreaController {
         	modelMap.put("errMsg", e.toString());
         }
         
+        logger.error("test error!");
+        long endTime=System.currentTimeMillis();
+        logger.debug("costTime:[{}ms]",endTime-startTime);
+        logger.info("======end======");
         return modelMap;
 	}
 
